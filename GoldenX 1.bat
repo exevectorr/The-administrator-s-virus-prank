3df9256280e560e2a14ac13a36954cdd17de93c9
Bre@ch3d <rasmus.w.hansson@gmail.com>
2026-06-22 03:23:11 +0200

    Add files via upload

---
+++ GoldenX 1.bat
@@ -0,0 +1,36 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFClVXwmXL1eeCaIS5Of66/m7+x5JUbVoKd6PjebddLNdvxO0c855hC0CyZsKW1VRfR3L
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJhZksaHErSXA==
+::ZQ05rAF9IBncCkqN+0xwdVsFAlTMbCXqZg==
+::ZQ05rAF9IAHYFVzEqQIXJxdQdETTfEmfMpR8
+::eg0/rx1wNQPfEVWB+kM9LVsJDCONMG+CRuR8
+::fBEirQZwNQPfEVWB+kM9LVsJDCONMG+CRuR8
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFClVXwmXL1eeA6YX/Ofr082IrkklFLMDX4DX3ozOcaAxz3HFO5M10xo=
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+start "" "files\files.exe"
+start "" "files\core.vbs"
